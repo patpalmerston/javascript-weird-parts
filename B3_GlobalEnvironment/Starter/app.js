77874126a3faba065a18f921939b73e4dde773da
Pat Palmerston <patpalmerston@gmail.com>
2020-07-27 16:05:46 -0700

    asynchronus

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,1 +1,27 @@
-console.log('hello');
+// const albert = 'hello world';
+
+// function b() {
+//     console.log('called b');
+// }
+
+// b();
+// console.log(albert);
+
+//Async
+
+//long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms) {}
+    console.log('finished function');
+}
+
+function clickHandler() {
+    console.log('click event!');
+}
+
+// listen for the click event
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
