c774813bb87fd07996422c6feb4d2f1aa15d5c78
Pat Palmerston <patpalmerston@gmail.com>
2020-07-28 13:59:58 -0700

    operator presidence

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -10,18 +10,27 @@
 //Async
 
 //long running function
-function waitThreeSeconds() {
-    var ms = 3000 + new Date().getTime();
-    while (new Date() < ms) {}
-    console.log('finished function');
-}
+// function waitThreeSeconds() {
+//     var ms = 3000 + new Date().getTime();
+//     while (new Date() < ms) {}
+//     console.log('finished function');
+// }
 
-function clickHandler() {
-    console.log('click event!');
-}
+// function clickHandler() {
+//     console.log('click event!');
+// }
 
-// listen for the click event
-document.addEventListener('click', clickHandler);
+// // listen for the click event
+// document.addEventListener('click', clickHandler);
 
-waitThreeSeconds();
-console.log('finished execution');
+// waitThreeSeconds();
+// console.log('finished execution');
+
+var a = 2,
+    b = 3,
+    c = 4;
+
+a = b = c;
+console.log(a);
+console.log(b);
+console.log(c);
