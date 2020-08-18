6826b21eaade5bdb71bd31993a93987df5dc1b77
Pat Palmerston <patpalmerston@gmail.com>
2020-08-17 17:02:04 -0700

    greeting methods on prototype

--- B3_GlobalEnvironment/Starter/Greetr.js
+++ B3_GlobalEnvironment/Starter/Greetr.js
@@ -3,7 +3,43 @@
         return new Greetr.init(firstName, lastName, language);
     };
 
-    Greetr.prototype = {};
+    var supportedLangs = ['en', 'es'];
+
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola',
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos',
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inicio sesion',
+    };
+
+    // any methods inside here, the 'this' variable will have access to when creating a new greetr object
+    Greetr.prototype = {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        validate: function () {
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw 'invalid Language';
+            }
+        },
+
+        greeting: function () {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreeting: function () {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+    };
 
     Greetr.init = function (firstName, lastName, language) {
         var self = this;
